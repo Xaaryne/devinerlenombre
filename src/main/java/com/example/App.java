9a442abfb9ed8b7374bc1e458dd0bbad107a8235
Rayane <rayaneelasraoui@gmail.com>
2022-11-24 09:04:36 +0100

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,28 +8,24 @@ public class App {
 
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
-        System.out.println("Je pense a un nombre entre 1 et 10. Devinez lequel");
+        System.out.println("Je pense a un nombre entre 1 et 100. Devinez lequel");
         Random generateur = new Random();
-        int chiffre = generateur.nextInt(10) + 1;
+        int chiffre = generateur.nextInt(100) + 1;
         int win = 0;
         int tentatives = 0;
         while (win != 1) {
             tentatives = tentatives + 1;
-            System.out.println("Donnez un nombre entre 1 et 10");
+            System.out.println("Donnez un nombre entre 1 et 100");
             int chiffredonne = clavier.nextInt();
-            int difference = chiffre - chiffredonne;
-            int chiffrepluspetit = chiffredonne - chiffre;
             if (chiffredonne == chiffre) {
                 System.out.println("Vous avez gagné !");
                 System.out.println("Il vous a fallu " + tentatives + " tentatives");
                 win = 1;
             } else if (chiffredonne > chiffre) {
-                System.out.println("Perdu ! La bonne réponse était " + chiffre);
-                System.out.println("Vous avez dépassé de " + chiffrepluspetit);
+                System.out.println("trop grand");
                 win = 0;
             } else if (chiffredonne < chiffre) {
-                System.out.println("Perdu ! La bonne réponse était " + chiffre);
-                System.out.println("Il vous manquait " + difference);
+                System.out.println("Trop petit");
                 win = 0;
             }
         }
