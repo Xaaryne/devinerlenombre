91907ab6abdd44385bd85678b7c390e7d63922e8
Rayane <rayaneelasraoui@gmail.com>
2022-11-24 08:42:16 +0100

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -11,20 +11,27 @@ public class App {
         System.out.println("Je pense a un nombre entre 1 et 10. Devinez lequel");
         Random generateur = new Random();
         int chiffre = generateur.nextInt(10) + 1;
-        System.out.println("Donnez un nombre entre 1 et 10");
-        int chiffredonne = clavier.nextInt();
-        int difference = chiffre - chiffredonne;
-        int chiffrepluspetit = chiffredonne - chiffre;
-        clavier.close();
-        if (chiffredonne == chiffre) {
-            System.out.println("Vous avez gagné !");
-        } else if (chiffredonne > chiffre) {
-            System.out.println("Perdu ! La bonne réponse était " + chiffre);
-            System.out.println("Vous avez dépassé de " + chiffrepluspetit);
-        } else if (chiffredonne < chiffre) {
-            System.out.println("Perdu ! La bonne réponse était " + chiffre);
-            System.out.println("Il vous manquait " + difference);
+        int win = 0;
+        int tentatives = 0;
+        while (win != 1) {
+            tentatives = tentatives + 1;
+            System.out.println("Donnez un nombre entre 1 et 10");
+            int chiffredonne = clavier.nextInt();
+            int difference = chiffre - chiffredonne;
+            int chiffrepluspetit = chiffredonne - chiffre;
+            if (chiffredonne == chiffre) {
+                System.out.println("Vous avez gagné !");
+                System.out.println("Il vous a fallu " + tentatives + " tentatives");
+                win = 1;
+            } else if (chiffredonne > chiffre) {
+                System.out.println("Perdu ! La bonne réponse était " + chiffre);
+                System.out.println("Vous avez dépassé de " + chiffrepluspetit);
+                win = 0;
+            } else if (chiffredonne < chiffre) {
+                System.out.println("Perdu ! La bonne réponse était " + chiffre);
+                System.out.println("Il vous manquait " + difference);
+                win = 0;
+            }
         }
     }
-
 }
